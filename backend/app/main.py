b119e7d209d734dfb4159db46e6e4725d75d7710
Mischif-Palta <chaitanyapalta@gmail.com>
2026-09-04 00:39:36 +0530

    Fix production frontend CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -21,11 +21,11 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://127.0.0.1:5500",
-        "https://your-vercel-url.vercel.app"
-    ],
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5500",
+    "https://concierge-commerce.vercel.app"
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
